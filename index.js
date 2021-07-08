9d400264f5263eb84cb4edf6b7e2becd07bfe31f
José López <jlopez-2019044@kinal.edu.gt>
2021-07-08 08:57:02 -0600

    se cambio index

--- index.js
+++ index.js
@@ -5,19 +5,8 @@ const app = require('./app')
 
 mongoose.Promise = global.Promise;
 
-const USER = 'root';
-const PASS = 'root';
-const PORT = 27017;
-const DB = 'TorneoDeportivo';
-const haveCredentials = false;
-let URL = '';
+const URL = 'mongodb+srv://root:root@tornaisse.j89lr.mongodb.net/tornaisseDB?retryWrites=true&w=majority';
 
-if( haveCredentials ) {
-    URL = `mongodb://${USER}:${PASS}@localhost:${PORT}/${DB}?authSource=admin`;
-
-} else {
-    URL = 'mongodb+srv://root:root@tornaisse.j89lr.mongodb.net/tornaisseDB?retryWrites=true&w=majority';
-}
 
 mongoose.connect( URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }).then(() => {
     console.log('Successful database connection')
